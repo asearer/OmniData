2b273858f9a03b0c394ed70e51f541fd239128ff
asearer <amsearer84@gmail.com>
2025-12-09 04:59:37 -0500

    feat: Add MIT license and .dockerignore, and refactor XML parsing for generic node handling.

--- internal/formats/xml.go
+++ internal/formats/xml.go
@@ -17,15 +17,20 @@ func init() {
 	})
 }
 
+// Node represents a generic XML element to allow round-tripping arbitrary XML
+type Node struct {
+	XMLName xml.Name
+	Attrs   []xml.Attr `xml:",any,attr"`
+	Content []byte     `xml:",innerxml"`
+	Nodes   []Node     `xml:",any"`
+}
+
 // readXML reads XML data from the given path.
-// If path is empty, reads from os.Stdin (for "-").
-// Returns parsed data as interface{}.
 func readXML(path string) (interface{}, error) {
 	var f *os.File
 	var err error
 
 	if path == "" {
-		// STDIN support
 		f = os.Stdin
 	} else {
 		f, err = os.Open(path)
@@ -35,22 +40,21 @@ func readXML(path string) (interface{}, error) {
 		defer f.Close()
 	}
 
-	var data interface{}
-	if err := xml.NewDecoder(f).Decode(&data); err != nil {
+	// Try to decode as a generic Node to preserve structure
+	var node Node
+	if err := xml.NewDecoder(f).Decode(&node); err != nil {
 		return nil, fmt.Errorf("failed to decode XML from '%s': %w", path, err)
 	}
 
-	return data, nil
+	return node, nil
 }
 
-// writeXML writes data as XML to the given path.
-// If path is empty, writes to os.Stdout (for "-").
+// writeXML writes data back to XML.
 func writeXML(path string, data interface{}) error {
 	var f *os.File
 	var err error
 
 	if path == "" {
-		// STDOUT support
 		f = os.Stdout
 	} else {
 		f, err = os.Create(path)
@@ -60,11 +64,14 @@ func writeXML(path string, data interface{}) error {
 		defer f.Close()
 	}
 
+	if _, ok := data.(Node); !ok {
+		return fmt.Errorf("data is not a valid XML Node")
+	}
+
 	enc := xml.NewEncoder(f)
 	enc.Indent("", "  ")
 	if err := enc.Encode(data); err != nil {
 		return fmt.Errorf("failed to encode XML to '%s': %w", path, err)
 	}
-
 	return nil
 }
